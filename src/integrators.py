9454610c8b65987c9b909f81ef6ec23f3c80d1e6
William Davies <williamthadiusdavies@gmail.com>
2026-02-12 13:02:09 -0600

    Forward Euler integrator

--- src/integrators.py
+++ src/integrators.py
@@ -8,7 +8,14 @@ class Integrator:
     
 class EulerIntegrator(Integrator):
     def step(self, system, dt):
-        pass
+        acc = compute_accelerations(system.particles, system.E, system.B)
+
+        for p, a in zip(system.particles, acc):
+            if not p.alive:
+                continue
+
+            p.pos += p.vel * dt
+            p.vel += a * dt
 
 class RK4Integrator(Integrator):
     def step(self, system, dt):
