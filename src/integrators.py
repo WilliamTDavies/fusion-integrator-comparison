d4a6b6e8181183361c2acd58ef882972c8da8242
William Davies <williamthadiusdavies@gmail.com>
2026-02-12 13:51:15 -0600

    RK4 Integrator

--- src/integrators.py
+++ src/integrators.py
@@ -19,7 +19,34 @@ class EulerIntegrator(Integrator):
 
 class RK4Integrator(Integrator):
     def step(self, system, dt):
-        pass
+
+        s0 = deepcopy(system.particles) # Work on copies to prevent accidental data deletion
+
+        a1 = compute_accelerations(s0, system.E, system.B)
+        s1 = deepcopy(s0)
+        for p, a in zip(s1.particles, a1):
+            p.pos += p.vel * dt * 0.5
+            p.vel += a * dt * 0.5
+
+        a2 = compute_accelerations(s1, system.E, system.B)
+        s2 = deepcopy(s0)
+        for p, a in zip(s2.particles, a2):
+            p.pos += p.vel * dt * 0.5
+            p.vel += a * dt * 0.5
+
+        a3 = compute_accelerations(s2, system.E, system.B)
+        s3 = deepcopy(s0)
+        for p, a in zip(s3.particles, a3):
+            p.pos += p.vel * dt
+            p.vel += a * dt
+        
+        a4 = compute_accelerations(s3)
+
+        for i, p in enumerate(system.particles):
+            if not p.alive:
+                continue
+            p.pos += (s0[i].vel + 2 * s1[i].vel + 2 * s2[i].vel + s3[i].vel) * dt / 6
+            p.vel += (a1[i] + 2 * a2[i] + 2 * a3[i] + a4[i]) * dt / 6
 
 class VelocityVerletIntegrator(Integrator):
     def step(self, system, dt):
@@ -29,7 +56,7 @@ class VelocityVerletIntegrator(Integrator):
             if not p.alive:
                 continue
             p.pos += p.vel * dt + 0.5 * a * dt ** 2
-        
+
         # Velocity
         acc_new = compute_accelerations(system.particles, system.E, system.B)
         for p, a0, a1 in zip(system.particles, acc_new):
